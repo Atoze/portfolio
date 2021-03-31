dfb9719661ab49af1534f265c9881e893e43d5b1
atoze <wada@centsys.jp>
2021-04-01 03:38:36 +0900

    [update] updated styles of header-navigation

--- src/components/TheHeaderNavigation.vue
+++ src/components/TheHeaderNavigation.vue
@@ -1,7 +1,22 @@
 <template>
-  <nav id="nav">
-    <router-link to="#profile">Profile</router-link> |
-    <router-link to="#playground">Playground</router-link> |
-    <router-link to="#contact">Contact</router-link>
+  <nav id="nav" class="header-navigation">
+    <router-link to="#profile" class="navigation__item">Profile</router-link>
+    <router-link to="#playground" class="navigation__item">Playground</router-link>
+    <router-link to="#contact" class="navigation__item">Contact</router-link>
   </nav>
 </template>
+
+<style lang="scss" scoped>
+  .header-navigation {
+    text-align: right;
+    padding: 24px max(8px, env(safe-area-inset-left)) 24px max(8px, env(safe-area-inset-left)) ; 
+  }
+  .navigation__item {
+    display: inline-block;
+    margin-right: 24px;
+    transition: transform 0.3s;
+    &:hover {
+      transform: translateY(-2px);
+    }
+  }
+</style>

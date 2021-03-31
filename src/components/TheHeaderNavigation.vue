5170de23505d9f43e7dd15b3e9727c24f222feec
atoze <wada@centsys.jp>
2021-04-01 05:33:58 +0900

    [update]navigation is fixed if window width is below 400px

--- src/components/TheHeaderNavigation.vue
+++ src/components/TheHeaderNavigation.vue
@@ -1,10 +1,56 @@
 <template>
-  <nav id="nav" class="header-navigation">
+  <nav id="nav"
+    class="header-navigation"
+    :class="{ 'mobile': state.isMobile }"
+  >
     <router-link to="#profile" class="navigation__item">Profile</router-link>
     <router-link to="#playground" class="navigation__item">Playground</router-link>
     <router-link to="#contact" class="navigation__item">Contact</router-link>
   </nav>
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive, onMounted } from "vue";
+
+export default defineComponent({
+  name: "TheHeaderNavigation",
+  setup(){
+    const state = reactive({
+      isMobile: false,
+    });
+
+    const checkIsMobile = () => {
+      if (window.innerWidth > 400) {
+        if (state.isMobile) {
+          state.isMobile = false;
+        }
+      } else {
+        if (!state.isMobile) {
+          state.isMobile = true;
+        }
+      }
+    };
+
+    onMounted(() => {
+      const mql = window.matchMedia("(min-width: 400px)");
+      mql.addListener(mql => {
+        if (mql.matches) {
+          if (state.isMobile) {
+            state.isMobile = false;
+          }
+        } else {
+          if (!state.isMobile) {
+            state.isMobile = true;
+          }
+        }
+      });
+      checkIsMobile();
+    });
+
+    return { state };
+  }
+});
+</script>
 
 <style lang="scss" scoped>
   .header-navigation {
@@ -13,6 +59,10 @@
     width: 100%;
     padding: 24px max(8px, env(safe-area-inset-left)) 24px max(8px, env(safe-area-inset-left)); 
     text-align: right;
+
+    &.mobile {
+      position: fixed;
+    }
   }
   .navigation__item {
     display: inline-block;
